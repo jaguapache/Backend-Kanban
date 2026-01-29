6dc338401dc1e316f917418122371c9f7a65a789
Alejandro G <jaguapache@viewnext.com>
2026-01-29 09:37:00 +0100

    feat: agregar manejo de excepciones para argumentos de tipo inválido en el GlobalExceptionHandler

--- src/main/java/com/example/springboot/kanban/kanban_app/config/GlobalExceptionHandler.java
+++ src/main/java/com/example/springboot/kanban/kanban_app/config/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -13,7 +14,14 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body("El cuerpo de la petición es inválido o contiene valores incorrectos.");
+                .body("El cuerpo de la petición es inválido o contiene valores incorrectos: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<String> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body("El argumento proporcionado es de un tipo inválido");
     }
 
 }
