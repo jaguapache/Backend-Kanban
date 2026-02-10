626f430dde755ec9d8251bf0f87c0741e7c85aff
Alejandro G <jaguapache@viewnext.com>
2026-02-10 19:48:02 +0100

    feat: Add Role model and update User model with authentication fields and role relationships

---
+++ src/main/java/com/example/springboot/kanban/kanban_app/models/Role.java
@@ -0,0 +1,35 @@
+package com.example.springboot.kanban.kanban_app.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
--- src/main/java/com/example/springboot/kanban/kanban_app/models/User.java
+++ src/main/java/com/example/springboot/kanban/kanban_app/models/User.java
@@ -1,11 +1,22 @@
 package com.example.springboot.kanban.kanban_app.models;
 
+import java.util.List;
+
+import com.example.springboot.kanban.kanban_app.validation.ExistsByUsername;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "users")
@@ -17,21 +28,43 @@ public class User {
 
     @Column(nullable = false)
     private String name;
+
     @Column(nullable = false)
     private String lastname;
-    @Column(nullable = false)
+
+    @Column(nullable = false, unique = true)
+    @ExistsByUsername
+    @NotBlank
     private String email;
+
+    @Column(nullable = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    @Transient
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private boolean admin;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private boolean enabled;
+
+    @ManyToMany
+    @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"), uniqueConstraints = {
+            @UniqueConstraint(columnNames = { "user_id", "role_id" }) })
+    private List<Role> roles;
+
     @Column(nullable = false)
     private Integer ubication;
 
     public User() {
     }
 
-    public User(Long id, String name, String lastname, String email, Integer ubication) {
+    public User(Long id, String name, String lastname, String email, String password, Integer ubication) {
         this.id = id;
         this.name = name;
         this.lastname = lastname;
         this.email = email;
+        this.password = password;
         this.ubication = ubication;
     }
 
@@ -73,6 +106,38 @@ public class User {
 
     public void setUbication(Integer ubication) {
         this.ubication = ubication;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
     }
 
     @Override
