349b59c54effee53e2bc7ca330f4510cb1974880
Alejandro G <jaguapache@viewnext.com>
2026-02-12 12:39:07 +0100

    feat: Eliminar importaciones no utilizadas en JpaUserDetailService

--- src/main/java/com/example/springboot/kanban/kanban_app/services/JpaUserDetailService.java
+++ src/main/java/com/example/springboot/kanban/kanban_app/services/JpaUserDetailService.java
@@ -1,8 +1,7 @@
 package com.example.springboot.kanban.kanban_app.services;
 
-import java.util.ArrayList;
+
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
